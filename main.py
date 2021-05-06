4174b777c0d7e158b7321d3c2872528007aeeffd
AmirBahrami9 <amirbahraami99@gmail.com>
2021-05-06 10:41:06 +0430

    add comment

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+# This script send desktop notification each 20 minutes to remind you a repetitive work
+
 import os
 import time
 
 for i in range(1, 6):
-    time.sleep(60 * 20) # second
-    os.system("notify-send \"Hello Programmer!\" \"Let's move your body :)\"")
+    time.sleep(60 * 20) #second
+    os.system("notify-send \"Hello Programmer!\" \"Let's move your body :)\"") #run command
+
+
