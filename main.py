83241d633d9d0f414534ce5e1e2d762ad29334f2
AmirBahrami9 <amirbahraami99@gmail.com>
2021-05-07 23:16:44 +0430

    times

--- main.py
+++ main.py
@@ -3,8 +3,20 @@
 import os
 import time
 
-for i in range(1, 6):
-    time.sleep(60 * 20) #second
-    os.system("notify-send \"Hello Programmer!\" \"Let's move your body :)\"") #run command
+from time_utils import *
 
+print("Note: Enter times in format 00:00:00", end = "\n\n")
 
+total = input("Enter total time: ")
+per = input("Enter alternation time: ")
+
+total_seconds = hr_to_sec(total[0:2]) + min_to_sec(total[3:5]) + int(total[6:8])
+
+per_seconds = hr_to_sec(per[0:2]) + min_to_sec(per[3:5]) + int(per[6:8])
+
+num = int(total_seconds / per_seconds)
+
+num += 1
+for i in range(1, num):
+    time.sleep(per_seconds)
+    os.system("notify-send \"Hello Programmer!\" \"Let's move your body :)\"")  # run command
---
+++ time_utils.py
@@ -0,0 +1,8 @@
+# some functions to works with times
+
+def min_to_sec(minutes):
+    return int(minutes) * 60
+
+
+def hr_to_sec(hour):
+    return int(hour) * 3600
