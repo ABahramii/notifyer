8e958162e62b6abd82d2c54f566e12252274f240
AmirBahrami9 <amirbahraami99@gmail.com>
2021-05-07 23:21:17 +0430

    update comments

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This script send desktop notification each 20 minutes to remind you a repetitive work
+# This script send desktop notification to remind you a repetitive work
 
 import os
 import time
@@ -10,10 +10,12 @@ print("Note: Enter times in format 00:00:00", end = "\n\n")
 total = input("Enter total time: ")
 per = input("Enter alternation time: ")
 
+#convert times to seconds
 total_seconds = hr_to_sec(total[0:2]) + min_to_sec(total[3:5]) + int(total[6:8])
 
 per_seconds = hr_to_sec(per[0:2]) + min_to_sec(per[3:5]) + int(per[6:8])
 
+# calculate number of repeats
 num = int(total_seconds / per_seconds)
 
 num += 1
